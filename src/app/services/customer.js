ddd3570dbceb2b75d2e424f9561679ffedbfea12
Gabriel <gabriel-borba@outlook.com>
2023-11-09 16:54:42 -0300

    feat: adicionando rotas

--- src/app/services/customer.js
+++ src/app/services/customer.js
@@ -1,45 +1,20 @@
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 const router = express.Router();
 
 const customerProxy = createProxyMiddleware({
-  target: 'http://localhost:8080/',
+  target: "http://localhost:8080/",
   changeOrigin: true,
 });
 
-router.get(`/`, (req, res) => {
-    customerProxy(req, res, (error) => {    
-      if (error) {
-        res.status(500).json({ error: 'Erro ao acessar o serviço de cliente' });
-      } else {
-        const customers = res.locals.data; 
-        res.json(customers);
-      }
-    });
-});
+router.post("/deposit", customerProxy);
+router.post("/withdraw", customerProxy);
+router.post("/transfer", customerProxy);
 
+router.get("/:id", customerProxy);
+router.get("/dashboard", customerProxy);
+router.get("/transactions", customerProxy);
 
-router.get('/:id', (req, res) => {
-  customerProxy(req, res, (error) => {
-    if (error) {
-      res.status(500).json({ error: 'Erro ao acessar o serviço de cliente' });
-    } else {      
-      const customer = res.locals.data; 
-      res.json(customer);
-    } 
-  })
-});
-
-router.post('/', (req, res) => {
-  customerProxy(req, res, (error) => {
-    if (error) {
-      res.status(500).json({ error: 'Erro ao acessar o serviço de cliente' });
-    } else {      
-      const customer = res.locals.data; 
-      res.json(customer);
-    } 
-  })
-});
-
+router.put("/profile/:id", customerProxy);
 
 module.exports = router;
